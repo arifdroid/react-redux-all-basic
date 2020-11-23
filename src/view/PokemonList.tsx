208e59268a84290d500014459af3e1a1bb8bcb64
arifdroid <arifdroiddeveloper@gmail.com>
2020-11-23 16:05:43 +0800

    passing and rendering

--- src/view/PokemonList.tsx
+++ src/view/PokemonList.tsx
@@ -3,38 +3,56 @@ import { useDispatch, useSelector } from 'react-redux';
 // https://stackoverflow.com/questions/55636963/property-does-not-exist-on-type
 import _ from 'lodash';
 import { GetPokemonList } from '../modules/pokemon-basic-redux/actions-basic/pokemonActions_basic';
-const PokemonList = ()=>{
+import { Link } from 'react-router-dom';
+const PokemonList = () => {
 
     const dispatch = useDispatch();
-    const pokemonList = useSelector((state:any)=> state.PokemonList );
+    const pokemonList = useSelector((state: any) => state.PokemonList);
 
 
-    useEffect(()=>{ 
+    useEffect(() => {
         //calll method that dispatch action
         FetchData(1)
-    },[])
+    }, [])
 
-    const FetchData = (page=1)=>{
+    const FetchData = (page = 1) => {
         //dispatch action         
         dispatch(GetPokemonList(page))
     }
 
     //listener to state changes
-    const showData = ()=>{
-        if(!_.isEmpty(pokemonList.data)){
-            return <p> have data </p>
+    const showData = () => {
+        if (!_.isEmpty(pokemonList.data)) {
+
+            // console.log('data is ',pokemonList.data)
+            return (
+                <div>
+                    <p>have data </p>
+                    <br></br>
+                    {
+                        pokemonList.data.results.map((el: any, index: any) => (
+                            <div key={index}>
+                                <br />
+                                <p> pokemon name is :{el.name}</p>
+                                <Link to={`/poke-list/${el.name}`}>View {index}</Link>
+                            </div>
+
+                        ))
+                    }
+                </div>
+            )
         }
 
-        if( pokemonList.loading){
+        if (pokemonList.loading) {
             return <p>loading ...</p>
         }
 
-        if(pokemonList.errorMessage !== ''){
+        if (pokemonList.errorMessage !== '') {
             return <p>{ } </p>
         }
     }
-    
-    return(
+
+    return (
 
         <div >
 
@@ -43,7 +61,7 @@ const PokemonList = ()=>{
             {showData()}
 
         </div>
-    
+
     )
 }
 
